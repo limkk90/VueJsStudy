f0a9b5af995480d507f708e60659696f5e9cf5b2
limkk90 <44752388+limkk90@users.noreply.github.com>
2021-11-11 23:39:05 +0900

    parent component

--- src/views/ParentComponent.vue
+++ src/views/ParentComponent.vue
@@ -1,19 +1,15 @@
-<template>
- <child-component @send-message="sendMessage" ref="child_component" />
- <button type="button" @click="changeChildData">Change !!!</button>
+<template lang="">
+  <child-component @send-message="sendMessage" ref="child_component" />
 </template>
 <script>
 import ChildComponent from './ChildComponent';
 export default {
- components: {ChildComponent},
- mounted() {
-   this.$refs.child_component.$refs.btn.click();
-   this.$refs.child_component.callFromParent();
- },
- methods: {
-     changeChildData(){
-         this.$refs.child_component.msg = "부모 컴포넌트가 변경한 데이터";
-     }
- },
+  components : {ChildComponent},
+  mounted(){
+    this.$refs.child_component.$refs.btn.click();
+  }
 }
 </script>
+<style lang="">
+  
+</style>
