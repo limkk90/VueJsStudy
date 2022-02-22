3f60d9259000924f70e25f535308490e97c7b33f
limkk90 <44752388+limkk90@users.noreply.github.com>
2022-02-22 22:31:08 +0900

    aaa

--- src/views/DataBindingSelect.vue
+++ src/views/DataBindingSelect.vue
@@ -4,6 +4,7 @@
             <option value="02">서울</option>
             <option value="21">부산</option>
             <option value="064">제주</option>
+            <option value="065">제주1</option>
         </select>
     </div>
 </template>
